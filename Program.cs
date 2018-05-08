9f363d272a71e5a84eb23bf6af68206f7876a005
Sam Haycraft <shaycraft@metlife.com>
2018-05-07 21:25:52 -0600

    Can't believe I got it

--- Program.cs
+++ Program.cs
@@ -2,11 +2,38 @@
 
 namespace lookandsay
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private const int NUM_ITERATIONS = 10;
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string number = "1";
+            for (int iter = 0; iter < NUM_ITERATIONS; iter++)
+            {
+                string newNumber = "";
+                int digit = Int32.Parse(number.Substring(0, 1));
+                int currDigit = digit;
+                int digitCount = 0;
+                for (int i = 0; i < number.Length; i++)
+                {
+                    ++digitCount;
+                    currDigit = Int32.Parse(number.Substring(i, 1));
+                    if (currDigit != digit)
+                    {
+                        newNumber += calculate_digits(digit, digitCount - 1);
+                        digit = currDigit;
+                        digitCount = 1;
+                    }
+                }
+                newNumber += calculate_digits(currDigit, digitCount);
+                number = newNumber;
+                Console.WriteLine(number);
+            }
+        }
+
+        private static string calculate_digits(int digit, int count)
+        {
+            return count.ToString() + digit.ToString();
         }
     }
 }
